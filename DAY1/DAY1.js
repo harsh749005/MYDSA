6ae66c360934715e94cb5fce5cb99bd8a41f305e
Harsh <patelharsh749005@gmail.com>
2025-07-12 22:18:14 +0530

    DAY1 problems are solved

--- DAY1/DAY1.js
+++ DAY1/DAY1.js
@@ -1,4 +1,56 @@
-let num1 = Number(prompt("Enter the first number : "));
-let num2 = Number(prompt("Enter the second number : "));
-let sum = num1 + num2;
-alert("Your answer",sum);
+// Addition of 2 number
+// let num1 = Number(prompt("Enter the first number : "));
+// let num2 = Number(prompt("Enter the second number : "));
+// let sum = num1 + num2;
+// alert("Your answer",sum);
+
+// Print Name and Age
+// let name = prompt("Enter your name ");
+// let age = Number(prompt("Enter the age"));
+// console.log(`Name : ${name} and Age : ${age}`);
+
+// Area of Rectangle
+// let a = Number(prompt("Enter the length : "));
+// let b = Number(prompt("Enter the width : "));
+// console.log(`Legnth = ${a}, width = ${b}, and their area is = ${a*b}`);
+
+//Find greatest number using ternary operator
+// let num1 = Number(prompt("Enter the first number : "));
+// let num2 = Number(prompt("Enter the second number : "));
+// console.log(num1 > num2 ? `${num1} is greater than ${num2}`:`${num2} is greater than ${num1}`)
+
+// Even or odd
+// let num = Number(prompt("Enter the number : "));
+// console.log(num % 2 === 0 ? `${num} is Even number ` : ` ${num } is Odd number`);
+
+// Eligible to Vote
+// let age = Number(prompt("Enter the age : "));
+// console.log(age < 18 ? "Not aligible to vote" : "Eligible to vote");
+
+// Print day of the week based on number
+// let day = Number(prompt("Enter the number from 1-7 : "));
+// switch (day) {
+//   case 1:
+//     console.log("Moday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("Ahh ! You enter something else ....");
+// }
