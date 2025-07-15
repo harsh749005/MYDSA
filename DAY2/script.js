2d461c9f4fbc1091c30a0c4aaa814554e07eb23f
Harsh <patelharsh749005@gmail.com>
2025-07-15 22:23:29 +0530

    DAY-2 is done

---
+++ DAY2/script.js
@@ -0,0 +1,32 @@
+// Area of Triangle
+// let base = Number(prompt("Enter the bredth : "));
+// let width = Number(prompt("Enter the width : "));
+// let area = 1/2 * base * width;
+// console.log(`Area of Triangle is : ${area}sq.units`)
+
+// Greater Number 
+// let num1 = Number(prompt("Enter the first number : "));
+// let num2 = Number(prompt("Enter the second number : "));
+// console.log(`${num1 > num2 ? num1 +" is greater than "+ num2:num2 +" is greater than "+ num1 }`)
+
+// Number is odd or even
+// let num = Number(prompt("Enter the number : "));
+// console.log(`${num % 2 === 0 ? num +" is even number ": num + " odd number "}`);
+
+// Leap Year
+// let year = Number(prompt("Enter the year : "));
+// if(year % 4 ===0 ){
+//     if(year % 100 === 0 ){
+//         if(year % 400 === 0 ){
+//             console.log(year + " is a leap year");
+//         }else{
+//             console.log(year + " is not a year");
+//         }
+//     }
+//     else{
+//         console.log(year + "is a leap year");
+//     }
+// }
+// else{
+//     console.log(year + " is not leap year")
+// }
